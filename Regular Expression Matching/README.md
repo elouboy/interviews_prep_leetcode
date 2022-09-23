f0a87ddb7d9936d1894dc864a49b617f5ea958a8
Elhadj Ndiaye <elou95@gmail.com>
2022-09-23 20:28:36 +0000

    regular exp matching finish

---
+++ Regular Expression Matching/README.md
@@ -0,0 +1,2 @@
+# 10. Regular Expression Matching
+[click Here](https://leetcode.com/problems/regular-expression-matching/) to see the problem
---
+++ Regular Expression Matching/regular_exp_matching.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+ var isMatch = function(s, p) {
+    const [count] = s.match(p) || [];
+    return count === s;
+};
